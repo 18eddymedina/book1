a26cb2689d425619325d3c1461f939c2d26fe38b
18eddymedina <18emedina@gmail.com>
2019-03-19 18:28:52 -0500

    initial

---
+++ client/build/precache-manifest.f22391f13f3fb4bbc57bca2580639a08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "18b07753f60c8bb07bb2",
+    "url": "/static/js/main.18e08603.chunk.js"
+  },
+  {
+    "revision": "eb2a08a850b75d39c542",
+    "url": "/static/js/2.1118b730.chunk.js"
+  },
+  {
+    "revision": "18b07753f60c8bb07bb2",
+    "url": "/static/css/main.4f4dee85.chunk.css"
+  },
+  {
+    "revision": "1e8751b54c8061e264c15bcc5786045c",
+    "url": "/index.html"
+  }
+];
